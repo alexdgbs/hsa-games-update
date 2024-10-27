f80bb9adaddb75a95a04390a252b8e1bbc2190d8
alexdgbs <alexdbc24@gmail.com>
2024-10-27 14:45:19 -0600

    Update vercel

--- dist/_nuxt/static/1730059176/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Fteam","\u002F","\u002Fcheckout","\u002Fconsolas","\u002Fcancel","\u002Fgamecube","\u002Finicio","\u002Femuladores","\u002Fgba","\u002Fpong","\u002Fn64","\u002Fps2","\u002Fsuccess","\u002Fregister","\u002Fsnes","\u002Fprecios","\u002Flogin","\u002Fpsp"]})
